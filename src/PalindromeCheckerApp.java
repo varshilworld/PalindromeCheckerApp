eddc8461080e4175277857ffbeb9537d838fbf3c
varshilworld <varshilgupta@gmail.com>
2026-02-12 10:25:18 +0530

    [200~UC1: Application Entry & Welcome Message

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,11 @@
+public class PalindromeCheckerApp {
+
+    /*
+    UC1
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome to Palindrome Checker Management System");
+        System.out.println("Version : 1.0");
+        System.out.println("System Initialised Successfully");
+    }
+}
