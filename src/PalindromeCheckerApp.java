bd84b1a6d6ff95e2e8a473818b95303b4ba31eda
varshilworld <varshilgupta@gmail.com>
2026-02-19 10:19:26 +0530

    use case 2 improved

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -7,16 +7,22 @@ public class PalindromeCheckerApp {
         String input = "madam";
         boolean palindrome = true;
 
+        // Loop through half the string
         for (int i = 0; i < input.length() / 2; i++) {
             if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
                 palindrome = false;
                 break;
             }
         }
-
+// Print the original string
+        System.out.println(input);
+        
+        // Print result
         if (palindrome)
             System.out.println("Palindrome");
         else
             System.out.println("Not Palindrome");
+
+
     }
 }
