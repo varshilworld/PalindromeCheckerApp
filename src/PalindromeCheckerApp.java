d4f577a2116d57b4d39eaa03cdf3b0bf648a253a
varshilworld <varshilgupta@gmail.com>
2026-02-19 11:14:37 +0530

    use case 3

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -5,23 +5,33 @@ public class PalindromeCheckerApp {
      */
     public static void main(String[] args) {
         String input = "madam";
-        boolean palindrome = true;
 
-        // Loop through half the string
-        for (int i = 0; i < input.length() / 2; i++) {
-            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
-                palindrome = false;
-                break;
-            }
+
+        String front = "";
+        for (int i = 0; i < input.length(); i++) {
+            front += input.charAt(i); // add each character
+        }
+        System.out.println("Front collected: " + front);
+
+
+        String reverse = "";
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reverse += input.charAt(i);
         }
 
-        System.out.println(input);
-        
-        if (palindrome)
-            System.out.println("Palindrome");
+        System.out.println("Reverse collected: " +reverse);
+        if (reverse.equals(front)) {
+            System.out.println("Palindrome");}
         else
             System.out.println("Not Palindrome");
 
 
-    }
-}
+
+
+
+        }
+        }
+
+
+
+
