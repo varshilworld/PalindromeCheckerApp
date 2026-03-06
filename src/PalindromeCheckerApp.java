a4f9a481e43dc0f9a135b6ece68fb5dd81d1b694
varshilworld <varshilgupta@gmail.com>
2026-03-06 08:20:56 +0530

    usercase 9

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -5,32 +5,45 @@ import java.util.Stack;
 public class PalindromeCheckerApp {
 
     /*
-    UC7
+    UC9
      */
 
 
-    
+
 
         public static void main(String[] args) {
 
-            String input = "level";
+            String input = "madam";
 
-            LinkedList<Character> list = new LinkedList<>();
-
-            for (char c : input.toCharArray()) {
-                list.add(c);
-            }
-
-            boolean isPalindrome = true;
-
-            while (list.size() > 1) {
-                if (list.removeFirst() != list.removeLast()) {
-                    isPalindrome = false;
-                    break;
-                }
-            }
+            boolean result = check(input, 0, input.length() - 1);
 
             System.out.println("Input : " + input);
-            System.out.println("Is Palindrome? : " + isPalindrome);
+            System.out.println("Is Palindrome? : " + result);
+        }
+
+        private static boolean check(String s, int start, int end) {
+
+            if (start >= end)
+                return true;
+
+            if (s.charAt(start) != s.charAt(end))
+                return false;
+
+            return check(s, start + 1, end - 1);
         }
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
